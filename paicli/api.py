df8174f6c7649ca420be1ae82724618f397281a5
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2018-06-09 17:23:48 +0800

    add mock functions of api

--- paicli/api.py
+++ paicli/api.py
@@ -12,6 +12,20 @@ class API(object):
     def __init__(self, api_info):
         self.api_info = api_info
 
+    def post_token(self):
+        pass
+
+    def put_user(self):
+        pass
+
+    def delete_user(self):
+        """Admin only."""
+        pass
+
+    def put_user_username_virtualclusters():
+        """Admin only."""
+        pass
+
     def get_jobs(self, username=""):
         url = "{}/api/{}/jobs".format(self.api_info.uri, self.api_info.version)
 
@@ -23,6 +37,15 @@ class API(object):
         else:
             print(res.raise_for_status())
 
+    def get_jobs_jobname(self, jobname):
+        pass
+
+    def post_jobs(self):
+        pass
+
+    def get_jobs_jobname_config(self, jobname):
+        pass
+
     def get_jobs_jobname_ssh(self, job_name):
         uri = "{}/api/{}/jobs/{}/ssh".format(self.api_info.uri, self.api_info.version, job_name)
         res = requests.get(uri)
@@ -32,4 +55,11 @@ class API(object):
         else:
             print(res.raise_for_status())
 
+    def put_jobs_jobname_executiontype(self, jobname):
+        pass
 
+    def get_virtualclusters(self):
+        pass
+
+    def get_virtualclusters_vcname(self, vcname):
+        pass
