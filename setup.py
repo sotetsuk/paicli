01c4aa47740ed7d555811a8a9088f054abfa7ecb
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2018-06-11 12:12:48 +0800

    update version: fix minor bugs especially for py3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paicli',
-    version="0.2.0",
+    version="0.2.1",
     description='Client for PAI',
     author='Sotetsu KOYAMADA',
     url='',
