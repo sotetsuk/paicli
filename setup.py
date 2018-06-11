303122f5b6491c8ea3168204462611ba808ec0df
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2018-06-11 11:28:36 +0800

    version up for temporary release in team

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paicli',
-    version="0.1.0",
+    version="0.2.0",
     description='Client for PAI',
     author='Sotetsu KOYAMADA',
     url='',
