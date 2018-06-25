12493d9a802c0c90783d08435a456ca7824ed8f8
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2018-06-25 15:10:29 +0800

    version up to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paicli',
-    version="0.2.2",
+    version="0.3.0",
     description='Client for PAI',
     author='Sotetsu KOYAMADA',
     url='',
