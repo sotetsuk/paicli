fd668cbeced0545a36db699416df701e4b0063c3
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2018-06-26 14:21:17 +0800

    fix debug message

--- paicli/utils.py
+++ paicli/utils.py
@@ -3,23 +3,16 @@ logger = logging.getLogger(__name__)
 
 
 def to_str(inp):
-    logger.debug("Before casting")
-    logger.debug(inp)
-    logger.debug(type(inp))
+    _type = type(inp)
     try:
         unicode # python2
-        logger.debug("python2")
         inp = inp.decode('utf-8')
     except NameError:
-        logger.debug("python3")
         try:
             inp = str(inp, 'utf-8')
         except TypeError:
-            logger.debug("str in py3. cast was not executed.")
             pass
 
-    logger.debug("After casting")
-    logger.debug(inp)
-    logger.debug(type(inp))
+    logger.debug("Casted \"{}\" from {} to {}".format(inp, _type, type(inp)))
 
     return inp
